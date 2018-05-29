6f7d37c3c577ff717ce36628d0b41496c36e1504
Andre Ippisch <ippisch@cs.uni-duesseldorf.de>
2018-05-29 14:18:33 +0200

    Fix testing

--- alias.sh
+++ alias.sh
@@ -3,5 +3,5 @@
 # gcc -Wall -std=c99 something.c -o program
 echo -e '#!/bin/bash\n/usr/bin/gcc -Wall -std=c99 "$@"' > /usr/bin/gcca && chmod +x /usr/bin/gcca
 
-# gcc -Wall -std=c99 -g -fprofile-arcs -ftest-coverage -pthread -lcheck -lrt -lm -lsubunit tests-for-something.c something.c -o tests
-echo -e '#!/bin/bash\n/usr/bin/gcc -Wall -std=c99 -g -fprofile-arcs -ftest-coverage -pthread -lcheck -lrt -lm -lsubunit "$@"' > /usr/bin/gcct && chmod +x /usr/bin/gcct
+# gcc -Wall -std=c99 tests-for-something.c something.c -o tests -g -fprofile-arcs -ftest-coverage -pthread -lcheck -lrt -lm -lsubunit
+echo -e '#!/bin/bash\n/usr/bin/gcc -Wall -std=c99 "$@" -g -fprofile-arcs -ftest-coverage -pthread -lcheck -lrt -lm -lsubunit' > /usr/bin/gcct && chmod +x /usr/bin/gcct
