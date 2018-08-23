b12828ef148f213663e5bc8c9487660b414d5d32
Andre Ippisch <ippisch@cs.uni-duesseldorf.de>
2018-08-23 10:41:21 +0200

    Update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -eq 0 ] ; then
-    docker run --name hardprog-container --rm -it -u $(id -u):$(id -g) -v "$PWD":/data -w /data hardprog-image
+    docker run --name hardprog-container --rm -it -u $(id -u):$(id -g) -v "$PWD":/data -w /data hhucn/hardprog
 else
-    docker run --name hardprog-container --rm --user $(id -u):$(id -g) -v "$PWD":/data -w /data hardprog-image "$@"
+    docker run --name hardprog-container --rm --user $(id -u):$(id -g) -v "$PWD":/data -w /data hhucn/hardprog "$@"
 fi
